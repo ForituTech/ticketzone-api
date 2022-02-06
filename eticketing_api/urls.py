54f81a2ce9f09b8de1a080dcb10256d554aa8d89
mogendi <mogendi.mongare@gmail.com>
2022-02-06 23:54:28 +0300

    setup swagger-ui docs

--- eticketing_api/urls.py
+++ eticketing_api/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+
+from rest_framework.schemas import get_schema_view
 
 admin.site.site_header = "Eticketing Super-Admin"
 admin.site.site_title = "Eticketing Super-Admin Portal"
@@ -23,4 +26,13 @@ admin.site.index_title = "Welcome to Eticketing Super-Admin Portal"
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("events/", include("events.urls")),
+    path('openapi', get_schema_view(
+        title="Eticketing-API",
+        description="Engine for ticket management …",
+        version="0.1.0"
+    ), name='openapi-schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
