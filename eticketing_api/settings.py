f892852c7c660c58bf2e2c8b5690bd9deb121493
mogendi <mogendi.mongare@gmail.com>
2022-03-05 22:19:10 +0300

    postgres switch

--- eticketing_api/settings.py
+++ eticketing_api/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = "eticketing_api.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["POSTGRES_DB"],
+        'USER': os.environ["POSTGRES_USER"],
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
+        'HOST': os.environ["POSTGRES_SERVER"],
+        'PORT': os.environ["POSTGRES_PORT"],
     }
 }
 
