084c6feeb5edb0d942e25a2fac3dca8578d29c07
mogendi <40548350+mogendi@users.noreply.github.com>
2022-03-09 15:19:26 +0300

    Heroku setup (#26)
    * python version correction
    
    * heroku apt-file
    
    * heroku apt-file
    
    * heroku apt-file
    
    * heroku apt-file
    
    * heroku apt-file
    
    * heroku apt-file

--- eticketing_api/wsgi.py
+++ eticketing_api/wsgi.py
@@ -1,7 +1,8 @@
 import os
 
+from dj_static import Cling  # noqa
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eticketing_api.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
