24248d8d7ad116b0e48ff3a78f8941b4506e6ff6
mogendi <40548350+mogendi@users.noreply.github.com>
2022-03-09 03:19:07 +0300

    Heroku setup (#23)
    * python version correction
    
    * python version correction
    
    * python version fix again
    
    * python version fix again

--- eticketing_api/settings.py
+++ eticketing_api/settings.py
@@ -146,7 +146,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+SITE_ROOT = os.path.join(os.path.dirname(__file__), "..")
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(SITE_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
