89e30f3333bb0888115de2a4a06723e76e31d609
mogendi <40548350+mogendi@users.noreply.github.com>
2022-09-13 21:53:21 +0300

    use send_ticket_email as task (#115)

--- tickets/admin.py
+++ tickets/admin.py
@@ -2,6 +2,7 @@ from typing import Any, Sequence
 
 from django.contrib import admin
 
+from eticketing_api import settings
 from notifications.utils import send_ticket_email
 from tickets.models import Ticket
 
@@ -9,8 +10,9 @@ from tickets.models import Ticket
 @admin.action(description="Send Ticket(s)")
 def send_tickets_manual(modeladmin: Any, r: Any, tickets: Sequence[Ticket]) -> Any:
     for ticket in tickets:
-        send_ticket_email(
-            ticket.id,
+        send_ticket_email.apply_async(
+            args=(ticket.id,),
+            queue=settings.CELERY_NOTIFICATIONS_QUEUE,
         )
 
 
