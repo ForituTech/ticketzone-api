f7102fb49fbab3cb4e538d95b091232b6c4f7682
mogendi <mogendi.mongare@gmail.com>
2023-02-23 18:00:47 +0300

    change docs link

--- eticketing_api/urls.py
+++ eticketing_api/urls.py
@@ -24,7 +24,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     re_path(
-        r"$",
+        r"v1/internal/docs",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
